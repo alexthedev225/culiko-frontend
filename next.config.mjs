6b3bd0a0973eabc739941805b3244999fabb2806
alexthedev225 <alexcode225@gmail.com>
2024-07-13 03:53:19 +0200

    Work in progress

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
-// next.config.mjs
 const nextConfig = {
-    images: {
-      domains: ['localhost'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['localhost'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
