9da87389947681322070c6dd29e57dea5c139275
alexthedev225 <alexcode225@gmail.com>
2024-07-13 04:31:33 +0200

    Work in progress

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,7 +1,14 @@
-"use client"
+"use client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactNode } from "react";
+
 const queryClient = new QueryClient();
-const Layout = ({ children }) => {
+
+interface LayoutProps {
+  children: ReactNode; // Typage des enfants
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <QueryClientProvider client={queryClient}>
