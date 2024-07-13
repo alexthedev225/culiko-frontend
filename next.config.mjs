94c126b2ff1b1e503199d26522d66d7a787a9728
alexthedev225 <alexcode225@gmail.com>
2024-07-13 07:44:02 +0200

    Work in progress

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,5 @@
 const nextConfig = {
-  images: {
-    domains: ['localhost', 'culiko.vercel.app'], // Ajoutez ici votre domaine déployé
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-    return config;
-  },
+  
 };
 
 export default nextConfig;
